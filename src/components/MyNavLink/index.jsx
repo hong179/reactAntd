6c227c2a055ff2ea75deb31238902f39c6bf0515
cca <cca@163.com>
2021-07-22 07:22:02 +0800

    MyNavLink packaging

---
+++ src/components/MyNavLink/index.jsx
@@ -0,0 +1,10 @@
+import React, { Children, Component } from 'react'
+import { NavLink } from 'react-router-dom'
+
+export default class MyNavLink extends Component {
+    render() {
+        return (
+            <NavLink activeClassName="hignlight" className="list-group-item" {...this.props}/>
+        )
+    }
+}
