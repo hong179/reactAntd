d8866fdf44195bd9ecff18dd37adbce097c7109b
cca <cca@163.com>
2021-07-27 08:05:06 +0800

    react route with params

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
-import {Route,Switch} from 'react-router-dom'
+import {Route,Switch,Redirect} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
-import MyNavLink from './components/MyNavLink/index'
+import Header from './components/Header'
+import MyNavLink from './components/MyNavLink'
 
 export default class App extends Component {
 	render() {
@@ -10,7 +11,7 @@ export default class App extends Component {
 			<div>
 				<div className="row">
 					<div className="col-xs-offset-2 col-xs-8">
-						<div className="page-header"><h2>React Router Demo</h2></div>
+						<Header/>
 					</div>
 				</div>
 				<div className="row">
@@ -22,8 +23,8 @@ export default class App extends Component {
 							<a className="list-group-item active" href="./home.html">Home</a> */}
 
 							{/* 在React中靠路由链接实现切换组件--编写路由链接 */}
-							<MyNavLink to="./about">About</MyNavLink>
-							<MyNavLink to="./home">Home</MyNavLink>
+							<MyNavLink to="/about">About</MyNavLink>
+							<MyNavLink to="/home">Home</MyNavLink>
 						</div>
 					</div>
 					<div className="col-xs-6">
@@ -33,6 +34,7 @@ export default class App extends Component {
 								<Switch>
 									<Route path="/about" component={About}/>
 									<Route path="/home" component={Home}/>
+									<Redirect to="/about"/>
 								</Switch>
 								
 							</div>
--- src/components/MyNavLink/index.jsx
+++ src/components/MyNavLink/index.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
-import { NavLink } from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 
 export default class MyNavLink extends Component {
     render() {
+        // console.log(this.props);
         return (
             <NavLink activeClassName="hignlight" className="list-group-item" {...this.props}/>
         )
