a3e549dd4bc26ca855a5c2429b3d48243dea644f
cca <cca@163.com>
2021-07-29 06:41:31 +0800

    search

--- src/pages/Home/Message/Detail/index.jsx
+++ src/pages/Home/Message/Detail/index.jsx
@@ -1,17 +1,32 @@
 import React, { Component } from 'react'
+import qs from 'querystring'
+
+let obj={name:'tom',age:18}
+console.log(qs.stringify(obj));
+
+let str='carName=奔驰&price=199'
+console.log(qs.parse(str))
 
 
 const DetailData=[
     {id:'01',content:"你好，中国"},
     {id:'02',content:"你好，尚硅谷"},
-    {id:'03',content:"你好，未来的自己"},
+    {id:'03',content:"你好，未来的自己"}
 ]
 export default class Detail extends Component {
     render() {
-        //接收params
-        const {id,title}=this.props.match.params
-        const findResult=DetailData.find((detailObj)=>{
-            return detailObj.id===id
+        console.log(this.props);
+        // 接收params
+        // const {id,title} = this.props.match.params
+
+
+        //接收search参数
+        const {search} = this.props.location
+        const {id,title} = qs.parse(search.slice(1))
+        
+          
+        const findResult = DetailData.find((detailObj)=>{
+            return detailObj.id === id
         })
         return (
             <ul>
--- src/pages/Home/Message/index.jsx
+++ src/pages/Home/Message/index.jsx
@@ -10,16 +10,18 @@ export default class Message extends Component {
       ]
     }
     render() {
-      const {messageArr} =this.state
+      const {messageArr} = this.state
         return (
             <div>
                   <ul>
                     {
                       messageArr.map((msgObj)=>{
-                        return(
+                        return (
                           <li key={msgObj.id}>
                             {/* 向路由组件传递params参数 */}
-                            <Link to={`/home/message/detail/${msgObj.id}/${msgObj.title}`}>{msgObj.title}</Link>
+                            {/* <Link to={`/home/message/detail/${msgObj.id}/${msgObj.title}`}>{msgObj.title}</Link> */}
+                            {/* 向路由组件传递search参数 */}
+                            <Link to={`/home/message/detail/?id=${msgObj.id}&title=${msgObj.title}`}>{msgObj.title}</Link>
                           </li>
                         )
                       })          
@@ -27,7 +29,9 @@ export default class Message extends Component {
                   </ul>
                   <hr/>
                   {/* 声明接收params参数 */}
-                  <Route path="/home/message/detail/:id/:title" component={Detail}/>
+                  {/* <Route path="/home/message/detail/:id/:title" component={Detail}/> */}
+                  {/* search参数无需声明接收，正常注册路由即可 */}
+                  <Route path="/home/message/detail" component={Detail}/>
             </div>
         )
     }
