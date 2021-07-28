eaeedf20a29cd0cdceca2d9bc63f618942fc8943
cca <cca@163.com>
2021-07-29 07:10:18 +0800

    state

--- src/pages/Home/Message/Detail/index.jsx
+++ src/pages/Home/Message/Detail/index.jsx
@@ -1,12 +1,5 @@
 import React, { Component } from 'react'
-import qs from 'querystring'
-
-let obj={name:'tom',age:18}
-console.log(qs.stringify(obj));
-
-let str='carName=奔驰&price=199'
-console.log(qs.parse(str))
-
+// import qs from 'querystring'
 
 const DetailData=[
     {id:'01',content:"你好，中国"},
@@ -21,13 +14,15 @@ export default class Detail extends Component {
 
 
         //接收search参数
-        const {search} = this.props.location
-        const {id,title} = qs.parse(search.slice(1))
-        
+        // const {search} = this.props.location
+        // const {id,title} = qs.parse(search.slice(1))
+
+        // 接收state参数
+        const {id,title} = this.props.location.state || {}
           
         const findResult = DetailData.find((detailObj)=>{
             return detailObj.id === id
-        })
+        }) || {}
         return (
             <ul>
                 <li>ID:{id}</li>
--- src/pages/Home/Message/index.jsx
+++ src/pages/Home/Message/index.jsx
@@ -20,8 +20,12 @@ export default class Message extends Component {
                           <li key={msgObj.id}>
                             {/* 向路由组件传递params参数 */}
                             {/* <Link to={`/home/message/detail/${msgObj.id}/${msgObj.title}`}>{msgObj.title}</Link> */}
+
                             {/* 向路由组件传递search参数 */}
-                            <Link to={`/home/message/detail/?id=${msgObj.id}&title=${msgObj.title}`}>{msgObj.title}</Link>
+                            {/* <Link to={`/home/message/detail/?id=${msgObj.id}&title=${msgObj.title}`}>{msgObj.title}</Link> */}
+
+                            {/* 向路由组件传递state参数 */}
+                            <Link to={{pathname:`/home/message/detail`,state:{id:msgObj.id,title:msgObj.title}}}>{msgObj.title}</Link>
                           </li>
                         )
                       })          
@@ -30,7 +34,11 @@ export default class Message extends Component {
                   <hr/>
                   {/* 声明接收params参数 */}
                   {/* <Route path="/home/message/detail/:id/:title" component={Detail}/> */}
+
                   {/* search参数无需声明接收，正常注册路由即可 */}
+                  {/* <Route path="/home/message/detail" component={Detail}/> */}
+
+                  {/* state参数无需声明接收，正常注册路由即可 */}
                   <Route path="/home/message/detail" component={Detail}/>
             </div>
         )
