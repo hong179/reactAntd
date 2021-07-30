dde384d7614e6e80d7d8e1a8b3cc82b8a64204ba
cca <cca@163.com>
2021-07-30 08:23:18 +0800

    push replace go forward back

--- src/pages/Home/Message/index.jsx
+++ src/pages/Home/Message/index.jsx
@@ -9,6 +9,40 @@ export default class Message extends Component {
         {id:"03",title:"消息3"}
       ]
     }
+    replaceShow = (id,title)=>{
+      //replace跳转+携带params参数
+      // this.props.history.replace(`/home/message/detail/${id}/${title}`)
+
+      //replace跳转+携带search参数
+      // this.props.history.replace(`/home/message/detail/?id=${id}&title=${title}`)
+
+      //replace跳转+携带state参数
+      this.props.history.replace(`/home/message/detail`,{id,title})
+    }
+
+    pushShow = (id,title)=>{
+      //push跳转+携带params参数
+      // this.props.history.push(`/home/message/detail/${id}/${title}`)
+
+      //push跳转+携带search参数
+      // this.props.history.push(`/home/message/detail/?id=${id}&title=${title}`)
+
+      //push跳转+携带state参数
+      this.props.history.push(`/home/message/detail`,{id,title})
+    }
+
+    back = ()=>{
+      this.props.history.goBack()
+    }
+
+    forward = ()=>{
+      this.props.history.goForward() 
+    }
+
+    go = ()=>{
+      this.props.history.go(2)
+    }
+
     render() {
       const {messageArr} = this.state
         return (
@@ -26,6 +60,9 @@ export default class Message extends Component {
 
                             {/* 向路由组件传递state参数 */}
                             <Link replace to={{pathname:`/home/message/detail`,state:{id:msgObj.id,title:msgObj.title}}}>{msgObj.title}</Link>
+
+                            &nbsp;<button onClick={()=>this.pushShow(msgObj.id,msgObj.title)}>push查看</button>
+                            &nbsp;<button onClick={()=>this.replaceShow(msgObj.id,msgObj.title)}>replace查看</button>
                           </li>
                         )
                       })          
@@ -40,6 +77,10 @@ export default class Message extends Component {
 
                   {/* state参数无需声明接收，正常注册路由即可 */}
                   <Route path="/home/message/detail" component={Detail}/>
+
+                  <button onClick={this.back}>回退</button> &nbsp;
+                  <button onClick={this.forward}>前进</button> &nbsp;
+                  <button onClick={this.go}>GO</button>
             </div>
         )
     }
